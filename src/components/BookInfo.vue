c746c8d4c6001b465f7239b7b1d41c0a03e5dc29
dongfeng916 <dongfeng916@163.com>
2018-08-26 12:52:01 +0800

    图书详情封面图片

---
+++ src/components/BookInfo.vue
@@ -0,0 +1,64 @@
+<template>
+<div class="bookinfo">
+    <div class="thumb">
+        <img class='back' 
+        :src="info.image"
+        mode = 'aspectFill'>
+        <img class='img'
+         :src="info.image"
+         mode = 'aspectFit'>
+         <div class="info">
+             <div class="title">
+                 {{info.title}}
+             </div>
+             <div class="author">
+                 {{info.author}}
+             </div>
+         </div>
+    </div>
+    
+</div>
+</template>
+
+<script>
+export default {
+  props: ["info"]
+};
+</script>
+
+<style lang='scss'>
+.bookinfo{
+    .thumb{
+        width: 750rpx;
+        height: 500rpx;
+        overflow: hidden;
+        position: relative;
+        .back{
+            filter: blur(10px);
+            width: 100%;
+        }
+        .img{
+            position: absolute;
+            width: 100%;
+            height: 300rpx;
+            left: 0;
+            top: 30rpx;
+        }
+        .info{
+            position: absolute;
+            color: white;
+            width: 100%;
+            left: 0;
+            top: 330rpx;
+            text-align: center;
+            .title{
+                font-size: 20px;
+            }
+            .author{
+                font-size: 14px;
+            }
+        }
+    }
+}
+</style>
+
