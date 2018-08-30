359ccff6461f73f597fd129458e016145f420722
dongfeng916 <dongfeng916@163.com>
2018-08-30 14:55:20 +0800

    城市定位和添加评论-1

--- src/pages/detail/Detail.vue
+++ src/pages/detail/Detail.vue
@@ -1,10 +1,29 @@
 <template>
 <div>
     <BookInfo :info='info'></BookInfo>
+    <div class="comment">
+      <textarea v-model='comment'
+                class="textarea"
+                :maxlength='100'
+                placeholder='请输入图书短评'></textarea>
+    
+      <div class="location">
+        地理位置：
+        <switch color='#EA5A49' :checked='location' @change='getGeo'></switch>
+        <span class="text-primary">{{location}}</span>
+        <!-- checked 显示switch组件是否选中 -->
+      </div>
+      <div class="phone">
+        手机型号：
+        <switch color='#EA5A49' :checked='phone' @change='getPhone'></switch>
+        <span class="text-primary">{{phone}}</span>
+      </div>
+      <button class="btn" @click='addComment'>评论</button>
+    </div>
 </div>   
 </template>
 <script>
-import { get } from "@/util";
+import { get,post,showModal} from "@/util";
 import BookInfo from "@/components/BookInfo";
 export default {
   components: {
@@ -13,23 +32,106 @@ export default {
   data() {
     return {
       bookid: "",
-      info: {}
+      info: {},
+      comment: "",
+      location: "",
+      phone: "",
+      userinfo:{}
     };
   },
   methods: {
+    async addComment(){
+      if(!this.comment){
+        return
+      }
+      //评论内容 手机型号 地理位置 图书id 用户的openid
+      const data = {
+        openid:this.userinfo.openId,
+        bookid:this.bookid,
+        comment:this.comment,
+        phone:this.phone,
+        location:this.location
+      }
+      try{
+        await post('/weapp/addcomment',data)
+        this.comment=''
+      }catch(e){
+        showModal('失败',e.msg)
+      }
+      
+
+    },
     async getDetail() {
       const info = await get("/weapp/bookdetail", { id: this.bookid });
       wx.setNavigationBarTitle({
         title: info.title
       });
       this.info = info;
+    },
+    getGeo(e) {
+      //HKZBZ-MOMKJ-TSUFI-KADU2-BG5EO-5WB6D
+      const key = "HKZBZ-MOMKJ-TSUFI-KADU2-BG5EO-5WB6D";
+      let url = "https://apis.map.qq.com/ws/geocoder/v1/";
+      if (e.target.value) {
+        wx.getLocation({
+          success: geo => {
+            wx.request({
+              url,
+              data: {
+                key,
+                location: `${geo.latitude},${geo.longitude}`,
+                output: "json"
+              },
+              success: res => {
+                if (res.data.state === 0) {
+                  this.location = res.data.result.address_component.city;
+                } else {
+                  this.location = "未知地点";
+                }
+              }
+            });
+          }
+        });
+      } else {
+        this.location = "";
+      }
+    },
+    getPhone(e) {
+      if (e.target.value) {
+        const phoneInfo = wx.getSystemInfoSync();
+        this.phone = phonInfo.model;
+      } else {
+        //没有选中
+        this.phone = "";
+      }
     }
   },
   mounted() {
     this.bookid = this.$root.$mp.query.id;
     this.getDetail();
+    const userinfo = wx.getStorageSync('userinfo')
+    if(userinfo){
+      this.userinfo = userinfo
+    }
   }
 };
 </script>
-<style>
+<style lang='scss'>
+.comment {
+  margin-top: 10px;
+  .textarea {
+    width: 730rpx;
+    height: 200rpx;
+    background: #eee;
+    padding: 10rpx;
+  }
+  .location {
+    margin-top: 10px;
+    padding: 5px 10px;
+  }
+  .phone {
+    margin-top: 10px;
+    padding: 5px 10px;
+  }
+}
 </style>
