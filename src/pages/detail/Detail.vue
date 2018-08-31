6622e9fa4929c1f6b88e12ca81e709795bc242d8
dongfeng916 <dongfeng916@163.com>
2018-08-31 13:02:25 +0800

    页面详情及评论

--- src/pages/detail/Detail.vue
+++ src/pages/detail/Detail.vue
@@ -1,7 +1,8 @@
 <template>
 <div>
     <BookInfo :info='info'></BookInfo>
-    <div class="comment">
+    <CommentList :comments='comments'></CommentList>
+    <div class="comment" v-if='showAdd'>
       <textarea v-model='comment'
                 class="textarea"
                 :maxlength='100'
@@ -20,14 +21,20 @@
       </div>
       <button class="btn" @click='addComment'>评论</button>
     </div>
+    <div v-else>
+        未登录或者已经评论过了
+    </div>
+    <button open-type="share" class="btn">转发给好友</button>
 </div>   
 </template>
 <script>
 import { get,post,showModal} from "@/util";
 import BookInfo from "@/components/BookInfo";
+import CommentList from '@/components/CommentList'
 export default {
   components: {
-    BookInfo
+    BookInfo,
+    CommentList
   },
   data() {
     return {
@@ -36,8 +43,22 @@ export default {
       comment: "",
       location: "",
       phone: "",
-      userinfo:{}
+      userinfo:{},
+      comments:[]
     };
+  },
+  computed:{
+    showAdd(){
+      //没有登录
+      if(!this.userinfo.openId){
+        return false
+      }
+      //评论页面里查到有自己的openid
+      if(this.comments.filter(v=>v.openid==this.userinfo.openId).length){
+        return false
+      }
+      return true
+    }
   },
   methods: {
     async addComment(){
@@ -55,6 +76,7 @@ export default {
       try{
         await post('/weapp/addcomment',data)
         this.comment=''
+        this.getComments()
       }catch(e){
         showModal('失败',e.msg)
       }
@@ -67,6 +89,10 @@ export default {
         title: info.title
       });
       this.info = info;
+    },
+    async getComments(){
+      const comments = await get('/weapp/commentlist',{bookid:this.bookid})
+      this.comments = comments.list||[]
     },
     getGeo(e) {
       //HKZBZ-MOMKJ-TSUFI-KADU2-BG5EO-5WB6D
@@ -109,6 +135,7 @@ export default {
   mounted() {
     this.bookid = this.$root.$mp.query.id;
     this.getDetail();
+    this.getComments()
     const userinfo = wx.getStorageSync('userinfo')
     if(userinfo){
       this.userinfo = userinfo
